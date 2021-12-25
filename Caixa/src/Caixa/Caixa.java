76d7066286e292214c51c21ef933f1b5e439bd6e
MarcosGOFavaretto <marcosfavaretto.dev@gmail.com>
2021-12-25 20:20:48 -0300

    :ok_hand:REFACTOR: Refatora a classe principal do projeto

--- Caixa/src/Caixa/Caixa.java
+++ Caixa/src/Caixa/Caixa.java
@@ -11,18 +11,23 @@ import javax.swing.JOptionPane;
 public class Caixa {
 
     public static void main(String[] args) {
-        String nomeDoProduto = "";
-        int quantidadeDoProduto = 0;
-        float valorUnitárioDoProduto = 0;
-        float descontoDaVenda = 0;
-        float valorTotalDaCompra = 0;
-        nomeDoProduto = JOptionPane.showInputDialog("Digite o nome do produto abaixo: ");
-        quantidadeDoProduto = Integer.parseInt(JOptionPane.showInputDialog("Digite a quantidade do produto abaixo: "));
-        valorUnitárioDoProduto = Float.parseFloat(JOptionPane.showInputDialog("Digite o valor unitário do produto abaixo: ").replace(",", "."));
+        String nomeProduto;
+        int quantidadeProduto;
+        float valorUnitarioProduto, descontoDaVenda, valorTotalDaCompra;
+        DecimalFormat mascaraTexto = new DecimalFormat("R$ #,##0.00");
+
+        nomeProduto = JOptionPane.showInputDialog("Digite o nome do produto abaixo: ");
+        quantidadeProduto = Integer.parseInt(JOptionPane.showInputDialog("Digite a quantidade do produto abaixo: "));
+        valorUnitarioProduto = Float.parseFloat(JOptionPane.showInputDialog("Digite o valor unitário do produto abaixo: ").replace(",", "."));
         descontoDaVenda = Float.parseFloat(JOptionPane.showInputDialog("Insira o desconto em % abaixo: ").replace("%", ""));
-        valorTotalDaCompra = quantidadeDoProduto * valorUnitárioDoProduto - (quantidadeDoProduto * valorUnitárioDoProduto * (descontoDaVenda / 100));
-        DecimalFormat mascaraDoTexto = new DecimalFormat("R$ #,##0.00");
-        JOptionPane.showMessageDialog(null, "Produto: " + nomeDoProduto + "\n" + "Quantidade: " + String.valueOf(quantidadeDoProduto) + "\n" + "Valor unitário: " + mascaraDoTexto.format(valorUnitárioDoProduto) + "\n" + "Desconto: " + String.valueOf(descontoDaVenda) + "% \n" + "VALOR TOTAL DA COMPRA: " + mascaraDoTexto.format(valorTotalDaCompra));
+        valorTotalDaCompra = quantidadeProduto * valorUnitarioProduto - (quantidadeProduto * valorUnitarioProduto * (descontoDaVenda / 100));
+
+        JOptionPane.showMessageDialog(null,
+                "Produto: " + nomeProduto + "\n"
+                + "Quantidade: " + String.valueOf(quantidadeProduto) + "\n"
+                + "Valor unitário: " + mascaraTexto.format(valorUnitarioProduto) + "\n"
+                + "Desconto: " + String.valueOf(descontoDaVenda) + "% \n"
+                + "VALOR TOTAL DA COMPRA: " + mascaraTexto.format(valorTotalDaCompra));
         System.exit(0);
     }
 }
